a3bd949ab26ab8b9ca39cb8f6846bb11a1645b00
Sardorkhon <upwake304@gmail.com>
2024-11-13 17:11:15 +0500

    third

---
+++ first_month/3/main.py
@@ -0,0 +1,75 @@
+# ism = input("isim ")
+# lastname = input("familiya ")
+# town = input("shaxar ")
+# shtate = input("viliyat ")
+# phon_number = input("telepon raqm ")
+# location = input("manzil ")
+# where_are_you_studing = input("qayerda o'qysan ")
+#
+# print(
+#     f"mening ismim {ism} famliliyam {lastname}"
+#     f"\nmen {town} yashayman va bu viloyat {shtate}"
+#     f"\ntelepin raqamim {phon_number} masnzilim {location} men {where_are_you_studing} o'qyman"
+# )
+
+# 1
+# number_sm = int(input("номер критинг "))
+# answer_km = number_sm / 100
+# print(int(answer))
+
+#2
+# number_kilo = int(input("номер критинг "))
+# answer_t = number_kilo / 1000
+# print(int(answer_t))
+
+# 3
+# number_bit = int(input("номер критинг "))
+# answer_bayt = number_bit / 1024
+# print(int(answer_bayt))
+# 4
+# a = int(input("a kesmani kriting "))
+# b = int(input("b kesmani kriting, be kesma a dan kichkrok bulsin "))
+#
+# ab = a / b
+# print(int(ab))
+# 5
+# a = float(input("a kesmani kriting "))
+# b = float(input("b kesmani kriting, be kesma a dan kichkrok bulsin "))
+#
+# ab = a % b
+# print(float(ab))
+# 6
+# number = int(input("ikki xonali son berring "))
+#
+# onliklar = number / 10
+# birliklar = number % 10
+# print(f"onlik {int(onliklar)} birlik {birliklar}")
+
+# 7
+# number = int(input("ikki xonali son berring "))
+#
+# onliklar = number / 10
+# birliklar = number % 10
+# print(f"summasi {int(onliklar + birliklar)}")
+
+# 8
+# number = int(input("ikki xonali son berring "))
+#
+# onliklar = int(number / 10)
+# birliklar = number % 10
+#
+# print(f"{birliklar}{onliklar}")
+
+# 9
+# n = int(input("uch xonali raqam "))
+# s = int(n / 100)
+# print(s)
+
+# 10
+# n = int(input("uch xonali raqam "))
+# y = int(n / 100)
+# o_1 = int(n % 100)
+# o_2 = int(o_1 / 10)
+# b = int(o_1 % 10)
+# print(y,o_2,b)
+
---
+++ first_month/4/main.py
@@ -0,0 +1,83 @@
+import math
+# 1
+# number = int(input("square")
+# print(f"perimeter == {number}")
+#2
+# square = int(input("veleu ")
+# print(f"sguare {square ** 2}")
+#3
+# primeter_1 = int(input("side "))
+# primeter_2 = int(input("side_2 "))
+#
+# print(f"square {(primeter_1 + primeter_2) * 2}")
+# print(f"square {primeter_1 * primeter_2}")
+from sys import flags
+
+# # 4
+# deameter = float(input("deamter "))
+# print(deameter * 3.14)
+
+
+# 5
+# a = float(input("value "))
+# c = a** 3
+# d = a**2
+# b = d * 6
+# print(f"volume = {c}, the surface ")
+
+# # 6
+# paraliped_a = int(input("add a value"))
+# paraliped_b = int(input("add b value"))
+# paraliped_c = int(input("add c value"))
+#
+# volume = paraliped_a * paraliped_b * paraliped_c
+# square = 2 * ((paraliped_a * paraliped_b) + (paraliped_b * paraliped_c) + (paraliped_c * paraliped_a) )
+# print(f"volume == {volume} , square == {square} ")
+
+
+# 7
+# r = float(input("R kriting "))
+# l = 2 * 3.14 * r
+# s = 3.14 * (r ** 2)
+# print(l,s)
+
+# 8
+# b_value = float(input("b: "))
+# a_value = float(input("a: "))
+#
+# print((a_value + b_value) / 2)
+
+# 9
+# a = int(input("a "))
+# b = int(input("b "))
+# ab = a * b
+# print(math.sqrt(ab))
+
+# 10
+# a = int(input("a "))
+# b = int(input("b "))
+# print(f"sum == {a+b}")
+# print(f"equpment == {a*b}")
+# print(f"degree a == {a**2}")
+# print(f"degree b == {b**2}")
+
+# 11
+# first_number = float(input("add the former number"))
+# second_number = float(input("add the latter number"))
+#
+# print(f"sum == {first_number + second_number}")
+# print(f"equpment == {first_number * second_number}")
+# print(f"first's modul == {abs(first_number)}")
+# print(f"second's modul == {abs(second_number)}")
+
+# 12
+# conner_3_a = int(input("add the number from a "))
+# conner_3_b = int(input("add the number from b "))
+# conner_3_c = math.sqrt((conner_3_a ** 2) + (conner_3_b ** 2 ))
+#
+# print(f"P == {math.ceil(conner_3_a + conner_3_b + conner_3_c)}")
+# print(f"c == {math.ceil(conner_3_c)}")
+
+
+
+
