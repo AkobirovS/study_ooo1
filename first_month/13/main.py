0ca29bc6762024cb123f778d012db1ef4590effc
Sardorkhon <upwake304@gmail.com>
2024-11-13 16:57:55 +0500

    modefy

--- first_month/13/main.py
+++ first_month/13/main.py
@@ -50,5 +50,35 @@
 # counts_2 = 1
 # counts_3 = 1
 # while number > counts:
-#
 # print(counts_3)
+# 5
+# def fibonacci(n):
+#     # Базовый случай: если n равно 0, то возвращаем 0
+#     if n == 0:
+#         return 0
+#     # Базовый случай: если n равно 1, то возвращаем 1
+#     elif n == 1:
+#         return 1
+#     # Рекурсивный случай: возвращаем сумму двух предыдущих чисел
+#     else:
+#         return fibonacci(n - 1) + fibonacci(n - 2)
+# # 6
+# numbers = int(input("add the number:: "))
+# arr = []
+# for i in range(3,numbers):
+#     if i % 1 == 0 and i%i == 0:
+#         arr.append(i)
+#     else:
+#         pass
+# print(arr)
+# 1
+# numbers = input("add the numbers")
+# arr = numbers.split(" ")
+# s = int(arr[1])
+# l = 0
+# # for i in range(len(numbers)):
+# #     l += i
+# print(arr)
+# print(type(arr[0]))
+# print(int(arr[0]))
+# print(type(arr[0]))lamda
