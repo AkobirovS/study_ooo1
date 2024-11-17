7967b6f8335415c5582ec280fbd3345d70c1b850
Sardorkhon <upwake304@gmail.com>
2024-11-17 17:27:57 +0500

    commit

--- second month/2/main.py
+++ second month/2/main.py
@@ -67,7 +67,6 @@
 # numbers = input("add the number n :: ")
 # print(numbers[::-1])
 # 8
-from traceback import walk_stack
 
 # arr = [4,5,7,8,6,9]
 # arr_2 = []
@@ -142,19 +141,106 @@ from traceback import walk_stack
 #     pass
 # print(nummber_1)
 
+# # 23
+# numbers = int(input("add the numbers :: "))
+# numbers_1 = 0
+# k = int(input("add the numbers k :: "))
+# l = int(input("add the numbers l :: "))
+# arr = list(range(1,numbers+1))
+# total_sum = sum(arr[:k]) + sum(arr[l:])
+# print(total_sum)
 
-numbers = int(input("add the numbers :: "))
-numbers_1 = 0
-k = int(input("add the numbers k :: "))
-l = int(input("add the numbers l :: "))
-arr = list(range(1,numbers+1))
-arr_1 = []
-if 0 <= l <= k <= numbers:
-    for i in arr:
-        if i != k and i != l:
-            arr_1.append(i)
-        else:
-            pass
-    for i in arr_1:
-        numbers_1 = numbers_1 + i
-print(numbers_1)
+# 23
+# 00000 "I cant "
+# numbers = int(input("add the numbers :: "))
+# numbers_1 = 0
+# k = int(input("add the numbers k :: "))
+# l = int(input("add the numbers l :: "))
+# arr = list(range(1,numbers+1))
+# arr_2 = []
+# arr_3 = list(range(1,k))
+# arr_4 = list(range(l+1,numbers))
+# arr_2.append(arr_3)
+# arr_2.append(arr_4)
+# print(arr_2)
+
+# # 24
+# number = [2,5,8,11,14]
+# counts = 0
+# counts_2 = 0
+# arr = []
+# while len(number)-1 > counts:
+#     if len(number) != counts:
+#         counts_2 = number[counts] - number[counts+1]
+#     else:
+#         pass
+#     arr.append(counts_2)
+#     counts += 1
+# if arr[0] == arr[1]:
+#     print(abs(arr[0]))
+# else:
+#     print(0)
+#
+# 25
+# number = [2,5,8,11,14]
+# counts = 0
+# counts_2 = 0
+# arr = []
+# while len(number)-1 > counts:
+#     if len(number) != counts:
+#         counts_2 = number[counts] - number[counts+1]
+#     else:
+#         pass
+#     arr.append(counts_2)
+#     counts += 1
+# if arr[0] == arr[1]:
+#     print(abs(arr[0]))
+# else:
+#     print(0)
+# #25
+# number = [2,4,8]
+# counts = 0
+# counts_2 = 0
+# arr = []
+# while len(number)-1 > counts:
+#     if len(number) != counts:
+#         counts_2 = number[counts+1] / number[counts]
+#     else:
+#         pass
+#     arr.append(counts_2)
+#     counts += 1
+# if arr[0] == arr[1]:
+#     print(abs(arr[0]))
+# else:
+#     print(0)
+#
+# # 26 ??? cant did
+# arr = [2,3,4,5,6,7]
+# arr_2 = []
+# counts_1 = 0
+# counts_2 = []
+# lens = len(counts_2)
+#
+# while len(arr) > counts_1:
+#     if arr[counts_1] % 2 == 0:
+#         counts_2.append(1)
+#         while lens > counts_1:
+#             if counts_2[counts_1] == 2:
+#
+#     elif arr[counts_1] % 2 != 0:
+#         counts_2.append(2)
+# # if arr == arr_2 :
+# #     print("tgri")
+# # else:
+# # #     print("not")
+# arr = [2,3,4,5,6,7]
+# lens = len(arr)
+# counts = 0
+# while lens > counts:
+#     if arr[counts] % 2 == 0 and arr[lens-2] % 2 == 0:
+#         print("tigri")
+#     elif arr[counts] % 2 != 0 and arr[lens-2] % 2 != 0:
+#         print("tigri")
+#     else:
+#         print(0)
+#     counts += 2
