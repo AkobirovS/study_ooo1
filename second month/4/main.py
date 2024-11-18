a13305bbe7d5ccdeff34f87674a80cfbd2508919
Sardorkhon <upwake304@gmail.com>
2024-11-18 16:54:42 +0500

    modefy

--- second month/4/main.py
+++ second month/4/main.py
@@ -1,3 +1,17 @@
+# # 1
+# arr = input("add the number by ' ': ")
+# by = [int(arr) for arr in arr.split()]
+# answer = []
+# for i in range(len(by)):
+#     sums = 0
+#     for j in range(len(by)):
+#         if i != j:
+#             sums += by[j]
+#     answer.append(sums)
+#
+# print(f"answer :: {answer}")
+
+
 # # 2
 # strs_1 = input("add the string:: ")
 # strs_2 = input("add the second string:: ")
@@ -21,19 +35,32 @@
 # strs = list(input("add str :: "))
 # l = len(strs)
 # print(arr[l:])
-
+#
 # # 4
 # str = list(input("add the voise :: ").lower())
+# l = []
 # str = sorted(str)
-# for
+# counts = 0
+# while len(str) > counts:
+#     x = str.count(str[counts])
+#     l.append(x)
+#     counts+=1
+# counts_1 = 0
+# while len(str) > counts_1:
+#     if max(l) == str.count(str[counts_1]):
+#         print(str[counts_1])
+#         break
+#     else:
+#         pass
+#     counts_1+=1
 # 5
 # arr = ["sardor","alixon","faruh"]
 # str = ""
 # for i in arr:
 #     str += f"hello {i} "
 # print(str)
-#
-# n = list(map(int, input("Sonlarni vergul bilan ajratib kiriting: ").split(',')))
+#6
+# n = list(map(int, input("add the numbers by ',': ").split(',')))
 #
 # a = float('-inf')
 # b = float('-inf')
@@ -56,22 +83,20 @@
 # result = (a + b) / (x + y)
 #
 # print(f"the big number :: {(a + b)}")
-# print(f"Eng kichik ikkita elementning yig'indisi :: {(x + y)}")
-# print(f"Nisbat :: {result:.2f}")
+# print(f"the smoller 2 number :: {(x + y)}")
+# print(f"answer :: {result:.2f}")
 #
-#7
-# from itertools import permutations
-#
-# num = [1, 2, 3, 4, 5, 6]
-#
-# a = permutations(num, 3)
-#
-# x = 0
-# for i in a:
-#     if sum(i) == 10:
-#         print(i)
-#         x += 1
-#
-# print(f"Umumiy holatlar soni: {x}")
+# 7
+num = [1, 2, 3, 4, 5, 6]
 
+x = 0
+
+for i in range(len(num)):
+    for j in range(i + 1, len(num)):
+        for k in range(j + 1, len(num)):
+            if num[i] + num[j] + num[k] == 10:
+                print((num[i], num[j], num[k]))
+                x += 1
+
+print(f"tens: {x}")
 
