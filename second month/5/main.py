387b4bb412dbe21da34b6430996fc61c8475d9da
Sardorkhon <upwake304@gmail.com>
2024-11-21 21:08:48 +0500

    modefy

--- second month/5/main.py
+++ second month/5/main.py
@@ -14,20 +14,53 @@
 #     else:
 #         pass
 # print(n)
-# 2
-# numbers = input("add the numbers by ' ' speac  " ).split(" ")
+# # 2
+# numbers = input("add the arr: ").split(" ")
 # numbers = [int(i) for i in numbers]
-# l = len(numbers)
-# counts = 0
-# while len(numbers) > counts:
-#     if numbers[counts] > numbers[l-(len(numbers)-1)]:
-#         numbers[counts] = numbers[l-(len(numbers)-1)]
-#     else:
-#         numbers[counts] = numbers[l-(len(numbers)-1)]
-#     counts+=1
-#     l -=1
+#
+# changed = True
+# while changed:
+#     changed = False
+#     for i in range(1, len(numbers)):
+#         if numbers[i] > numbers[i - 1]:
+#             numbers[i], numbers[i - 1] = numbers[i - 1], numbers[i]
+#             changed = True
+#
+# print("answer:", numbers)
+# # 3
+# numbers = input("add the numbers by ' ' :: ").split(" ")
+# numbers = [int(i) for i in numbers]
 # print(numbers)
-# 6
+# numbers = sorted(numbers)
+# counts = 1
+# counts_arr = []
+# if len(numbers) <= 10:
+#     for i in numbers:
+#         if i == counts:
+#             counts_arr.append(i)
+#             counts += 1
+#         else:
+#             pass
+#     counts = 0
+#     for i in counts_arr:
+#         counts += 1
+#         print(f"{counts}:{numbers.count(i) * "*"}")
+# else:
+#     print("add only 10 numbers !!!!")
+# # 4
+# number = list(map(int , input("add the number").split(" ")))
+# if len(number) <= 5:
+#     number = sorted(number)
+#     print(number[len(number)-1],number[len(number)-2])
+# else:
+#     print("errore add the right answer !!!!")
+# # 5
+# strs = input("add the string :: ")
+# if len(strs) <= 7:
+#     print(strs.count("cat"))
+# else:
+#     print("write onle 7 letters ")
+# # 6
 # name = list(input("add the name:: "))
 # counts = 0
 # while len(name) >= counts:
