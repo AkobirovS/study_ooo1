572a7bb041518451997124c59e2a72c16ced3c89
Sardorkhon <upwake304@gmail.com>
2024-11-25 11:10:41 +0500

    modefy

---
+++ second month/7/main.py
@@ -0,0 +1,38 @@
+# 1
+# lesn = int(input("how many county you wont add ? :: "))
+# dicts = {}
+# for i in range(lesn):
+#     name = input("add the name of country ::  ")
+#     stats = input("add the name of capital :: ")
+#     dicts[i] = dict(name=f"{stats}")
+#
+# print(dicts.items())
+#2
+# lesn = int(input("how many county you wont add ? :: "))
+# dicts = {
+#     "uzb":"tash",
+#     "rus":"mos"
+# }
+# for i in range(lesn):
+#     name = input("add the name of country ::  ")
+#     stats = input("add the name of capital :: ")
+#     dicts[name] = stats
+# for key , velue in dicts.items():
+#     print(key+"  "+velue)
+#
+# print(dicts.items())
+# 3
+# dicts = {
+#     "uzb":"tash"
+# }
+# print(dicts.items())
+# dicts["uzb"] = "sam"
+# print(dicts.items())
+# 4
+# dicts = {
+#     "uzb":"tash",
+#     "rus":"mos"
+# }
+# print(dicts.items())
+# dicts.pop("uzb")
+# print(dicts.items())
