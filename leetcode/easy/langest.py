9c94468fee76291e3dee900db6e98c58bef5392d
Sardorkhon <upwake304@gmail.com>
2024-12-06 15:14:55 +0500

    modefy

--- leetcode/easy/langest.py
+++ leetcode/easy/langest.py
@@ -1,11 +1,11 @@
-def langesFirst(strs: list[str]) -> str:
-    if not strs:
-        return  ""
-    osnowa = strs[0]
-    for i in strs[1:]:
-        while not i.startswith(osnowa):
-            osnowa = osnowa[:-1]
-            if not osnowa:
-                return ""
-    return osnowa
-print(langesFirst(["dog","racecar","car"]))
+# def langesFirst(strs: list[str]) -> str:
+#     if not strs:
+#         return  ""
+#     osnowa = strs[0]
+#     for i in strs[1:]:
+#         while not i.startswith(osnowa):
+#             osnowa = osnowa[:-1]
+#             if not osnowa:
+#                 return ""
+#     return osnowa
+# print(langesFirst(["dog","racecar","car"]))
