9946e5fb6d83f55d987761152a3d573b6864f5b6
Sardorkhon <upwake304@gmail.com>
2024-12-22 17:33:27 +0500

    modefy

--- studey_month/3_month/3_lesson/gpt_url.py
+++ studey_month/3_month/3_lesson/gpt_url.py
@@ -1,40 +1,143 @@
-# # # # # url = "https://zillow-working-api.p.rapidapi.com/autocomplete"
-# # # # import pandas
-# # # #
-# # # # a = pandas.read_excel("")]
+# # # # url = "https://zillow-working-api.p.rapidapi.com/autocomplete"
+# # # import pandas
 # # #
-# # #
-# # # def fibannachhi(a: int, b: int, c: int) -> int:
-# # #     if c == 1 :
-# # #         return a
-# # #     elif c == 2:
-# # #         return b
-# # #     else:
-# # #         counts = 3
-# # #         while counts <= c:
-# # #             a,b = b,a + b
-# # #             counts +=1
-# # #
-# # #         return b
-# # #
-# # #
-# # # print(fibannachhi(10,5,5))
+# # # a = pandas.read_excel("")]
 # #
 # #
-# # text = open("text.txt","w+",encoding="utf-8")
-# # text.write("sardorkhon!!!!")
-# # print(text.read())
-# # text.close()
+# # def fibannachhi(a: int, b: int, c: int) -> int:
+# #     if c == 1 :
+# #         return a
+# #     elif c == 2:
+# #         return b
+# #     else:
+# #         counts = 3
+# #         while counts <= c:
+# #             a,b = b,a + b
+# #             counts +=1
 # #
-# # test = open("text.txt","r",encoding="utf-8")
+# #         return b
 # #
-# # print(test.read())
+# #
+# # print(fibannachhi(10,5,5))
 #
-# import pandas as pd
 #
-# test = pd.read_excel('1111.xlsx')
-# lests = []
-# for i in test['Mavzular nomi']:
-#     lests.append(i)
+# text = open("text.txt","w+",encoding="utf-8")
+# text.write("sardorkhon!!!!")
+# print(text.read())
+# text.close()
 #
-# print(lests)
+# test = open("text.txt","r",encoding="utf-8")
+#
+# print(test.read())
+
+import pandas as pd
+
+data = {
+    'Name': ['Ali', 'Sara', 'Omar'],
+    'Age': [23, 25, 22],
+    'City': ['Tashkent', 'Samarkand', 'Bukhara']
+}
+
+df = pd.DataFrame(data)
+print(df)
+
+
+sardor = pd.read_excel('sardor.xlsx')
+print(sardor)
+
+
+import pandas as pd
+
+file_path = 'sardor.xlsx'
+sheet_name = 'Sheet1'
+
+data = pd.read_excel(file_path, sheet_name=sheet_name)
+
+print("Текущие данные из Excel:")
+print(data)
+
+new_data = {
+    'Имя': ['Али', 'Фатима'],
+    'Возраст': [20, 22],
+    'Город': ['Ташкент', 'Москва']
+}
+
+new_df = pd.DataFrame(new_data)
+
+data = pd.concat([data, new_df], ignore_index=True)
+
+data.to_excel(file_path, sheet_name=sheet_name, index=False)
+
+print("\nДанные успешно добавлены и сохранены!")
+
+# # # # url = "https://zillow-working-api.p.rapidapi.com/autocomplete"
+# # # import pandas
+# # #
+# # # a = pandas.read_excel("")]
+# #
+# #
+# # def fibannachhi(a: int, b: int, c: int) -> int:
+# #     if c == 1 :
+# #         return a
+# #     elif c == 2:
+# #         return b
+# #     else:
+# #         counts = 3
+# #         while counts <= c:
+# #             a,b = b,a + b
+# #             counts +=1
+# #
+# #         return b
+# #
+# #
+# # print(fibannachhi(10,5,5))
+#
+#
+# text = open("text.txt","w+",encoding="utf-8")
+# text.write("sardorkhon!!!!")
+# print(text.read())
+# text.close()
+#
+# test = open("text.txt","r",encoding="utf-8")
+#
+# print(test.read())
+
+import pandas as pd
+
+data = {
+    'Name': ['Ali', 'Sara', 'Omar'],
+    'Age': [23, 25, 22],
+    'City': ['Tashkent', 'Samarkand', 'Bukhara']
+}
+
+df = pd.DataFrame(data)
+print(df)
+
+
+sardor = pd.read_excel('sardor.xlsx')
+print(sardor)
+
+
+import pandas as pd
+
+file_path = 'sardor.xlsx'
+sheet_name = 'Sheet1'
+
+data = pd.read_excel(file_path, sheet_name=sheet_name)
+
+print("Текущие данные из Excel:")
+print(data)
+
+new_data = {
+    'Имя': ['Али', 'Фатима'],
+    'Возраст': [20, 22],
+    'Город': ['Ташкент', 'Москва']
+}
+
+new_df = pd.DataFrame(new_data)
+
+data = pd.concat([data, new_df], ignore_index=True)
+
+data.to_excel(file_path, sheet_name=sheet_name, index=False)
+
+print("\nДанные успешно добавлены и сохранены!")
