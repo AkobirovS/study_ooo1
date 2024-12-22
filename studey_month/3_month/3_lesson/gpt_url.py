3aa9c47e0fb04dd510964de70d42b75e77a5b414
Sardorkhon <upwake304@gmail.com>
2024-12-22 16:59:20 +0500

    modefy learn pandas !!!

--- studey_month/3_month/3_lesson/gpt_url.py
+++ studey_month/3_month/3_lesson/gpt_url.py
@@ -1,1 +1,40 @@
-url = "https://zillow-working-api.p.rapidapi.com/autocomplete"
+# # # # # url = "https://zillow-working-api.p.rapidapi.com/autocomplete"
+# # # # import pandas
+# # # #
+# # # # a = pandas.read_excel("")]
+# # #
+# # #
+# # # def fibannachhi(a: int, b: int, c: int) -> int:
+# # #     if c == 1 :
+# # #         return a
+# # #     elif c == 2:
+# # #         return b
+# # #     else:
+# # #         counts = 3
+# # #         while counts <= c:
+# # #             a,b = b,a + b
+# # #             counts +=1
+# # #
+# # #         return b
+# # #
+# # #
+# # # print(fibannachhi(10,5,5))
+# #
+# #
+# # text = open("text.txt","w+",encoding="utf-8")
+# # text.write("sardorkhon!!!!")
+# # print(text.read())
+# # text.close()
+# #
+# # test = open("text.txt","r",encoding="utf-8")
+# #
+# # print(test.read())
+#
+# import pandas as pd
+#
+# test = pd.read_excel('1111.xlsx')
+# lests = []
+# for i in test['Mavzular nomi']:
+#     lests.append(i)
+#
+# print(lests)
