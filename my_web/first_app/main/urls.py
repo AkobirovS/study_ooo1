22c04474aaf0a6b7c050c56f3966d21e5bc0c9a1
AkobirovS <upwake304@gmain.com>
2025-02-21 04:38:14 -0500

    modify

--- my_web/first_app/main/urls.py
+++ my_web/first_app/main/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
-from .views import index,blog,index_2
+from .views import index,blog,index_2,single_blog,flighter,team,elements,content
 
 app_name = "main"
 
 urlpatterns = [
     path("", index, name="index"),
     path('blog/', blog, name='blog'),
-    path("index_2/",index_2,name="index_2")
+    path("index_2/",index_2,name="index_2"),
+    path('singe_blog/',single_blog, name="single-blog"),
+    path('flighter/', flighter, name="flighter"),
+    path('team/',team,name="team"),
+    path('elements/', elements, name="elements"),
+    path('content/', content, name="content"),
 ]
--- my_web/first_app/main/views.py
+++ my_web/first_app/main/views.py
@@ -10,3 +10,18 @@ def blog(request):
 def index_2(request):
     context = {}
     return render(request,'index-2.html',context)
+def single_blog(request):
+    context = {}
+    return render(request, 'single-blog.html',context)
+def flighter(request):
+    context = {}
+    return render(request, 'fighter.html',context)
+def team(request):
+    context = {}
+    return render(request, 'team.html',context)
+def elements(request):
+    context = {}
+    return render(request, 'elements.html',context)
+def content(request):
+    context = {}
+    return render(request, 'contact.html',context)
